fd6365a5cc52df4fb98b93736acc949fb9c233cb
FelipeGuzmanSierra <97761783+FelipeGuzmanSierra@users.noreply.github.com>
2024-07-01 14:25:14 -0500

    Add scripts to process images review from notion

---
+++ src/execution/review_images/garbage_collector.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'logger'
+require 'json'
+
+require_relative '../../use_cases/review_images/garbage_collector'
+
+# Configuration
+params = {
+  table_name: ENV.fetch('REVIEW_IMAGES_TABLE'),
+  db_host: ENV.fetch('DB_HOST'),
+  db_port: ENV.fetch('DB_PORT'),
+  db_name: ENV.fetch('POSTGRES_DB'),
+  db_user: ENV.fetch('POSTGRES_USER'),
+  db_password: ENV.fetch('POSTGRES_PASSWORD')
+}
+
+# Process bot
+begin
+  bot = GarbageCollector::ReviewImages.new(params)
+
+  bot.execute
+rescue StandardError => e
+  Logger.new($stdout).info(e.message)
+end
