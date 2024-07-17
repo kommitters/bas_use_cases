30b6f015c7a070667ce0f7fb7dda5a40e8d85b10
Felipe Guzmán Sierra <97761783+FelipeGuzmanSierra@users.noreply.github.com>
2024-07-17 14:18:29 -0500

    Update environment variables loading config (#16)

--- scripts/update_container.sh
+++ scripts/update_container.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+ENV_FILE="/app/.env"
+
+# Load the environment variables from the .env file
+set -a
+source $ENV_FILE
+set +a
+
 export GEM_HOME="/app/vendor/bundle/ruby/3.3.0"
 export GEM_PATH="/app/vendor/bundle/ruby/3.3.0"
 
