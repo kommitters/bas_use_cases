03cdb138b6f5e77a1ad2d153c72a25a50f72ec0d
FelipeGuzmanSierra <97761783+FelipeGuzmanSierra@users.noreply.github.com>
2024-07-18 15:35:44 -0500

    Add schedule config

--- db/build_shared_storage.sql
+++ db/build_shared_storage.sql
@@ -123,3 +123,21 @@ CREATE TABLE IF NOT EXISTS do_billing (
         time zone DEFAULT CURRENT_TIMESTAMP,
         PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS web_availability (
+    id SERIAL NOT NULL,
+    "data" jsonb,
+    tag varchar(255),
+    archived boolean,
+    stage varchar(255),
+    status varchar(255),
+    error_message jsonb,
+    version varchar(255),
+    inserted_at timestamp
+    with
+        time zone DEFAULT CURRENT_TIMESTAMP,
+        updated_at timestamp
+    with
+        time zone DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id)
+);
