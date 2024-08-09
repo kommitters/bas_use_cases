777d8af26d5121b2f9a0e73365914699d09cf5a1
FelipeGuzmanSierra <97761783+FelipeGuzmanSierra@users.noreply.github.com>
2024-08-09 14:01:28 -0500

    Add bots for the OSPO maintenance use case

---
+++ spec/use_cases/ospo_maintenance/update_work_item_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative '../../../src/use_cases/ospo_maintenance/update_work_item'
+
+ENV['OSPO_MAINTENANCE_NOTION_USERS_DATABASE_ID'] = 'OSPO_MAINTENANCE_NOTION_USERS_DATABASE_ID'
+ENV['NOTION_SECRET'] = 'NOTION_SECRET'
+ENV['OSPO_MAINTENANCE_TABLE'] = 'OSPO_MAINTENANCE_TABLE'
+ENV['DB_HOST'] = 'DB_HOST'
+ENV['DB_PORT'] = 'DB_PORT'
+ENV['POSTGRES_DB'] = 'POSTGRES_DB'
+ENV['POSTGRES_USER'] = 'POSTGRES_USER'
+ENV['POSTGRES_PASSWORD'] = 'POSTGRES_PASSWORD'
+
+RSpec.describe Create::WorkItem do
+  before do
+    params = {
+      users_database_id: ENV.fetch('OSPO_MAINTENANCE_NOTION_USERS_DATABASE_ID'),
+      secret: ENV.fetch('NOTION_SECRET'),
+      table_name: ENV.fetch('OSPO_MAINTENANCE_TABLE'),
+      db_host: ENV.fetch('DB_HOST'),
+      db_port: ENV.fetch('DB_PORT'),
+      db_name: ENV.fetch('POSTGRES_DB'),
+      db_user: ENV.fetch('POSTGRES_USER'),
+      db_password: ENV.fetch('POSTGRES_PASSWORD')
+    }
+
+    @bot = Update::WorkItem.new(params)
+  end
+
+  context '.execute' do
+    before do
+      bas_bot = instance_double(Bot::UpdateWorkItem)
+
+      allow(Bot::UpdateWorkItem).to receive(:new).and_return(bas_bot)
+      allow(bas_bot).to receive(:execute).and_return({})
+    end
+
+    it 'should execute the bas bot' do
+      expect(@bot.execute).not_to be_nil
+    end
+  end
+end
---
+++ src/execution/ospo_maintenance/update_work_item.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'logger'
+
+require_relative '../../use_cases/ospo_maintenance/update_work_item'
+
+# Configuration
+params = {
+  users_database_id: ENV.fetch('OSPO_MAINTENANCE_NOTION_USERS_DATABASE_ID'),
+  secret: ENV.fetch('NOTION_SECRET'),
+  table_name: ENV.fetch('OSPO_MAINTENANCE_TABLE'),
+  db_host: ENV.fetch('DB_HOST'),
+  db_port: ENV.fetch('DB_PORT'),
+  db_name: ENV.fetch('POSTGRES_DB'),
+  db_user: ENV.fetch('POSTGRES_USER'),
+  db_password: ENV.fetch('POSTGRES_PASSWORD')
+}
+
+# Process bot
+begin
+  bot = Update::WorkItem.new(params)
+
+  bot.execute
+rescue StandardError => e
+  Logger.new($stdout).info(e.message)
+end
