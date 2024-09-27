8149285b13a64b566ce6121945d3b1c43d14e4b1
Felipe <97761783+FelipeGuzmanSierra@users.noreply.github.com>
2024-09-27 16:41:36 -0500

    Add telegram bot command processor

---
+++ telegram_app/bin/web_availability.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/web_availability'
+
+module WebAvailability
+  class Error < StandardError; end 
+
+  connection = {
+    host: ENV.fetch('DB_HOST'),
+    port: ENV.fetch('DB_PORT'),
+    dbname: "bas",
+    user: ENV.fetch('POSTGRES_USER'),
+    password: ENV.fetch('POSTGRES_PASSWORD')
+  }
+
+  token = ENV.fetch('TELEGRAM_BOT_TOKEN')
+
+  bot = Bots::WebAvailability.new(token, connection)
+
+  bot.execute
+end
