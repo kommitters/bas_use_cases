8e5fd4b521f5f43e293db8b49306934dc5ef0c0f
HumanPseudo <cadalagu@hotmail.com>
2024-10-29 20:28:48 -0500

    Update table names in telegram bot add website script

--- telegram_app/lib/services/add_website.rb
+++ telegram_app/lib/services/add_website.rb
@@ -12,31 +12,32 @@ module Services
     #
     def execute
       website_id = process_website
-      chat_id = process_chat
+      conversation_id = process_chat
 
-      insert_relation(website_id, chat_id)
+      insert_relation(website_id, conversation_id)
     end
 
     private
 
     def process_website
-      insert_item(WEBSITE_TABLE, WEBSITE_URL, config[:url])
+      insert_item(OBSERVED_WEBSITE_TABLE, WEBSITE_URL, config[:url])
 
-      website = query_item(WEBSITE_TABLE, WEBSITE_URL, config[:url])
+      website = query_item(OBSERVED_WEBSITE_TABLE, WEBSITE_URL, config[:url])
 
       website.values.first.first
     end
 
     def process_chat
-      insert_item(CHATS_IDS_TABLE, CHATS_IDS_ID, config[:chat_id])
+      insert_item(CONVERSATIONS_IDS_TABLE, CONVERSATIONS_IDS_ID, config[:conversation_id])
 
-      chat = query_item(CHATS_IDS_TABLE, CHATS_IDS_ID, config[:chat_id])
+      conversation = query_item(CONVERSATIONS_IDS_TABLE, CONVERSATIONS_IDS_ID, config[:conversation_id])
 
-      chat[0]['id']
+      conversation[0]['id']
     end
 
-    def insert_relation(website_id, chat_id)
-      query = "INSERT INTO #{RELATION_TABLE} (website_id, telegram_chat_id) VALUES (#{website_id}, #{chat_id});"
+    def insert_relation(website_id, conversation_id)
+      query = "INSERT INTO #{RELATION_TABLE}
+              (observed_website_id, conversation_id) VALUES (#{website_id}, #{conversation_id});"
 
       execute_query(query)
     end
