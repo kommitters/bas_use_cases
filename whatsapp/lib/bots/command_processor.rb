c8d9edeffef260265cbae683148ad40eea61ab95
Lorenzo Zuluaga <lorenzo.zuluaga@kommit.co>
2024-11-21 14:51:14 -0500

    Refactor command_processor bot

--- whatsapp/lib/bots/command_processor.rb
+++ whatsapp/lib/bots/command_processor.rb
@@ -18,24 +18,25 @@ module Bas
         body = read_response.data['message']
         options = process_options
 
-        case body
-        when '/add' then add
-        when '/remove' then remove
-        when '/list' then list
-        else unprocessable_response
+        begin
+          case body
+          when '/add' then add
+          when '/remove' then remove
+          when '/list' then list
+          else unprocessable_response
+        rescue
+          { error: { status: 422 } }
+        end
+
+        { success: { status: 200 } }
       end
 
       private
 
-      def response(token, conversation_id, message_body)
-        headers = {
-          "Content-Type" => "application/json",
-          "Authorization" => "Bearer #{token}"
-        }
-      
+      def response(conversation_id, message_body)
         response = HTTParty.post(
           'https://graph.facebook.com/v17.0/393901540484202/messages',
-          headers: headers,
+          headers: get_headers,
           body: {
             messaging_product: "whatsapp",
             recipient_type: "individual",
@@ -53,35 +54,25 @@ module Bas
 
       def add
         conversation_id = read_response.data['conversation_id']
-        puts 'Response: Waiting for URL'
-        puts response(TOKEN, conversation_id, 'Please send the URL of the website you want to monitor')
-        {
-          success: {
-            status: 200
-        }}
+        response(conversation_id, 'Please send the URL of the website you want to monitor')
       end
 
       def remove
         conversation_id = read_response.data['conversation_id']
         options = process_options
 
-        puts response(TOKEN, conversation_id, 'Please send the number of the website you want to remove')
-        {
-          success: {
-            status: 200
-        }}
+        response(conversation_id, 'Please send the number of the website you want to remove')
       end
 
       def list
         conversation_id = read_response.data['conversation_id']
         options = process_options
-        puts 'Received /list command'
-        request = list_websites(options, conversation_id)
-        puts "Your websites are: #{request}"
-        {
-          success: {
-            status: 200
-        }}
+
+        response = list_websites(options, conversation_id)
+
+        message_body = "Your websites are:\n#{response.each_with_index.map { |w, index| "#{index + 1}. #{w[:url]}" }.join("\n")}"
+
+        response(conversation_id, message_body)
       end
 
       def unprocessable_response
@@ -95,19 +86,16 @@ module Bas
         # Check if the body is a valid URL (simple check for a URL pattern)
         elsif body.match?(%r{\Ahttp(s)?://\S+\.\S+}) ? body : "https://#{body}"
           add_url(body, options, conversation_id)
-        else
-          {
-            error: {
-              status: 422
-          }}
         end
-        {
-          success: {
-            status: 200
-        }}
       end
 
-      
+      def get_headers
+        {
+          "Content-Type" => "application/json",
+          "Authorization" => "Bearer #{TOKEN}"
+        }
+      end
+
       def valid_url?(message)
         message.match?(%r{\Ahttp(s)?://\S+\.\S+}) ? message : "https://#{message}"
       end
@@ -122,19 +110,12 @@ module Bas
       end
 
       def list_websites(options, conversation_id)
-        token = ENV.fetch('WHATSAPP_TOKEN')
         config = {
           connection: options,
           conversation_id: conversation_id
         }
-        response = Services::ListWebsites.new(config).execute
-        message_body = "Your websites are:\n#{response.each_with_index.map { |w, index| "#{index + 1}. #{w[:url]}" }.join("\n")}"
-        puts message_body
-        puts response(token, conversation_id, message_body)
-        {
-          success: {
-            status: 200
-        }}
+
+        Services::ListWebsites.new(config).execute
       end
 
       def delete_website(index, conversation_id, options)
@@ -142,12 +123,12 @@ module Bas
           connection: options,
           conversation_id: conversation_id
         }
+
         result = Services::ListWebsites.new(config_list).execute
         website = result[index.to_i - 1][:url]
-        puts "Deleting website: #{website}"
         config = { connection: options, website: website, conversation_id: conversation_id }
+
         Services::RemoveWebsite.new(config).execute
-        200
       end
     end
   end
