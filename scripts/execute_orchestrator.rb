01ccd594e41f39e18c1895bc75bf6c710529cbba
Felipe Guzmán Sierra <97761783+FelipeGuzmanSierra@users.noreply.github.com>
2025-04-03 16:14:16 -0500

    Fix pto and do issues (#62)
    * Update schedules paths config
    
    * Update digital ocean bots warning criteria
    
    * Fix script execution permissions

--- scripts/execute_orchestrator.rb
+++ scripts/execute_orchestrator.rb
@@ -1,16 +1,14 @@
+#!/usr/local/bin/ruby
 # frozen_string_literal: true
 
-# !/usr/bin/env ruby
-
-require 'bundler/setup'
 require 'bas/orchestrator'
 require_relative '../src/use_cases_execution/schedules'
 
-# # ##
-# # # Main entry point for the orchestrator.
-# # # It will run the orchestrator and start
-# # # the execution of the scheduled scripts.
-# # # The schedules are defined in the UseCasesExecution::Schedules module.
+##
+# Main entry point for the orchestrator.
+# It will run the orchestrator and start
+# the execution of the scheduled scripts.
+# The schedules are defined in the UseCasesExecution::Schedules module.
 
 schedules = UseCasesExecution::Schedules.load
 orchestrator = Bas::Orchestrator::Manager.new(schedules)
--- src/use_cases_execution/schedules.rb
+++ src/use_cases_execution/schedules.rb
@@ -13,64 +13,61 @@ module UseCasesExecution
       constants.map { |const| const_get(const) }.flatten
     end
 
-    path_to_repo = '../../../../../../../../../'
-    scripts_path = "#{path_to_repo}src/use_cases_execution"
-
     BIRTHDAY_SCHEDULES = [
-      { path: "#{scripts_path}/birthday/fetch_birthday_from_notion.rb", time: ['12:40'] },
-      { path: "#{scripts_path}/birthday/format_birthday.rb", time: ['12:50'] },
-      { path: "#{scripts_path}/birthday/notify_birthday_in_discord.rb", time: ['13:00'] },
-      { path: "#{scripts_path}/birthday/garbage_collector.rb", time: ['00:00'] }
+      { path: "#{__dir__}/birthday/fetch_birthday_from_notion.rb", time: ['12:40'] },
+      { path: "#{__dir__}/birthday/format_birthday.rb", time: ['12:50'] },
+      { path: "#{__dir__}/birthday/notify_birthday_in_discord.rb", time: ['13:00'] },
+      { path: "#{__dir__}/birthday/garbage_collector.rb", time: ['00:00'] }
     ].freeze
 
     BIRTHDAY_NEXT_WEEK_SCHEDULES = [
-      { path: "#{scripts_path}/birthday_next_week/fetch_next_week_birthday_from_notion.rb", time: ['12:40'] },
-      { path: "#{scripts_path}/birthday_next_week/format_next_week_birthday.rb", time: ['12:50'] },
-      { path: "#{scripts_path}/birthday_next_week/notify_next_week_birthday_in_discord.rb", time: ['13:00'] },
-      { path: "#{scripts_path}/birthday_next_week/garbage_collector.rb", time: ['00:00'] }
+      { path: "#{__dir__}/birthday_next_week/fetch_next_week_birthday_from_notion.rb", time: ['12:40'] },
+      { path: "#{__dir__}/birthday_next_week/format_next_week_birthday.rb", time: ['12:50'] },
+      { path: "#{__dir__}/birthday_next_week/notify_next_week_birthday_in_discord.rb", time: ['13:00'] },
+      { path: "#{__dir__}/birthday_next_week/garbage_collector.rb", time: ['00:00'] }
     ].freeze
 
     DIGITAL_OCEAN_BILL_ALERT_SCHEDULES = [
-      { path: "#{scripts_path}/digital_ocean_bill_alert/fetch_billing_from_digital_ocean.rb", interval: 10_000 },
-      { path: "#{scripts_path}/digital_ocean_bill_alert/format_do_bill_alert.rb", interval: 10_000 },
-      { path: "#{scripts_path}/digital_ocean_bill_alert/notify_do_bill_alert_discord.rb", interval: 10_000 },
-      { path: "#{scripts_path}/digital_ocean_bill_alert/garbage_collector.rb", time: ['00:00'] }
+      { path: "#{__dir__}/digital_ocean_bill_alert/fetch_billing_from_digital_ocean.rb", interval: 10_000 },
+      { path: "#{__dir__}/digital_ocean_bill_alert/format_do_bill_alert.rb", interval: 10_000 },
+      { path: "#{__dir__}/digital_ocean_bill_alert/notify_do_bill_alert_discord.rb", interval: 10_000 },
+      { path: "#{__dir__}/digital_ocean_bill_alert/garbage_collector.rb", time: ['00:00'] }
     ].freeze
 
     PTO_SCHEDULES = [
-      { path: "#{scripts_path}/pto/fetch_pto_from_notion.rb", time: ['13:10'] },
-      { path: "#{scripts_path}/pto/humanize_pto.rb", time: ['13:20'] },
-      { path: "#{scripts_path}/pto/notify_pto_in_discord.rb", time: ['13:30'] },
-      { path: "#{scripts_path}/pto/garbage_collector.rb", time: ['00:00'] }
+      { path: "#{__dir__}/pto/fetch_pto_from_notion.rb", time: ['13:10'] },
+      { path: "#{__dir__}/pto/humanize_pto.rb", time: ['13:20'] },
+      { path: "#{__dir__}/pto/notify_pto_in_discord.rb", time: ['13:30'] },
+      { path: "#{__dir__}/pto/garbage_collector.rb", time: ['00:00'] }
     ].freeze
 
     PTO_NEXT_WEEK_SCHEDULES = [
-      { path: "#{scripts_path}/pto_next_week/fetch_next_week_pto_from_notion.rb", time: ['12:40'], day: ['Thursday'] },
-      { path: "#{scripts_path}/pto_next_week/humanize_next_week_pto.rb", time: ['12:50'], day: ['Thursday'] },
-      { path: "#{scripts_path}/pto_next_week/notify_next_week_pto_in_discord.rb", time: ['13:00'], day: ['Thursday'] },
-      { path: "#{scripts_path}/pto_next_week/garbage_collector.rb", time: ['00:00'], day: ['Thursday'] }
+      { path: "#{__dir__}/pto_next_week/fetch_next_week_pto_from_notion.rb", time: ['12:40'], day: ['Thursday'] },
+      { path: "#{__dir__}/pto_next_week/humanize_next_week_pto.rb", time: ['12:50'], day: ['Thursday'] },
+      { path: "#{__dir__}/pto_next_week/notify_next_week_pto_in_discord.rb", time: ['13:00'], day: ['Thursday'] },
+      { path: "#{__dir__}/pto_next_week/garbage_collector.rb", time: ['23:00'], day: ['Thursday'] }
     ].freeze
 
     SUPPORT_EMAIL_SCHEDULES = [
-      { path: "#{scripts_path}/support_email/fetch_emails_from_imap.rb", time: ['12:40', '14:40', '18:40', '20:40'] },
-      { path: "#{scripts_path}/support_email/format_emails.rb", time: ['12:50', '14:50', '18:50', '20:50'] },
-      { path: "#{scripts_path}/support_email/garbage_collector.rb", time: ['21:10'] },
-      { path: "#{scripts_path}/support_email/notify_support_emails.rb", time: ['13:00', '15:00', '19:00', '21:00'] }
+      { path: "#{__dir__}/support_email/fetch_emails_from_imap.rb", time: ['12:40', '14:40', '18:40', '20:40'] },
+      { path: "#{__dir__}/support_email/format_emails.rb", time: ['12:50', '14:50', '18:50', '20:50'] },
+      { path: "#{__dir__}/support_email/garbage_collector.rb", time: ['21:10'] },
+      { path: "#{__dir__}/support_email/notify_support_emails.rb", time: ['13:00', '15:00', '19:00', '21:00'] }
     ].freeze
 
     WIP_LIMIT_SCHEDULES = [
-      { path: "#{scripts_path}/wip_limit/fetch_domains_wip_count.rb", time: ['12:20', '14:20', '18:20', '20:20'] },
-      { path: "#{scripts_path}/wip_limit/fetch_domains_wip_limit.rb", time: ['12:30', '14:30', '18:30', '20:30'] },
-      { path: "#{scripts_path}/wip_limit/compare_wip_limit_count.rb", time: ['12:40', '14:40', '18:40', '20:40'] },
-      { path: "#{scripts_path}/wip_limit/garbage_collector.rb", time: ['21:10'] },
-      { path: "#{scripts_path}/wip_limit/format_wip_limit_exceeded.rb", time: ['12:50', '14:50', '18:50', '20:50'] },
-      { path: "#{scripts_path}/wip_limit/notify_domains_wip_limit_exceeded.rb",
+      { path: "#{__dir__}/wip_limit/fetch_domains_wip_count.rb", time: ['12:20', '14:20', '18:20', '20:20'] },
+      { path: "#{__dir__}/wip_limit/fetch_domains_wip_limit.rb", time: ['12:30', '14:30', '18:30', '20:30'] },
+      { path: "#{__dir__}/wip_limit/compare_wip_limit_count.rb", time: ['12:40', '14:40', '18:40', '20:40'] },
+      { path: "#{__dir__}/wip_limit/garbage_collector.rb", time: ['21:10'] },
+      { path: "#{__dir__}/wip_limit/format_wip_limit_exceeded.rb", time: ['12:50', '14:50', '18:50', '20:50'] },
+      { path: "#{__dir__}/wip_limit/notify_domains_wip_limit_exceeded.rb",
         time: ['13:00', '15:00', '19:00', '21:00'] }
     ].freeze
 
     SAVE_BACKUP = [
-      { path: "#{scripts_path}/save_backup/save_backup_in_r2.rb", time: ['00:00'] },
-      { path: "#{scripts_path}/save_backup/delete_older_backup_in_r2.rb", time: ['00:20'] }
+      { path: "#{__dir__}/save_backup/save_backup_in_r2.rb", time: ['00:00'] },
+      { path: "#{__dir__}/save_backup/delete_older_backup_in_r2.rb", time: ['00:20'] }
     ].freeze
   end
 end
