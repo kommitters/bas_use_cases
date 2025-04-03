51bbca467963e252bcc2c780c38e884455198c7f
Felipe <97761783+FelipeGuzmanSierra@users.noreply.github.com>
2025-04-03 10:28:40 -0500

    Update schedules paths config

--- scripts/update_container.sh
+++ scripts/update_container.sh
@@ -1,6 +1,9 @@
 
 # UPDATE DATABASE
+echo "UPDATE DATABASE"
 ruby /app/scripts/update_database.rb
+echo "DATABASE UPDATED"
 
 # UPDATE CRONJOBS
+echo "INITIALIZE CRONJOBS"
 ruby /app/scripts/execute_orchestrator.rb
