2e64cf5d7dcf2a7f7b91643d53d558f730a05c3f
kren-prog <karenpg40@gmail.com>
2025-05-19 10:20:03 -0500

    Add table to build_shared_storage

--- db/build_shared_storage.sql
+++ db/build_shared_storage.sql
@@ -265,3 +265,21 @@ CREATE TABLE IF NOT EXISTS apollo_sync (
         PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS repos_score (
+    id SERIAL NOT NULL,
+    "data" jsonb,
+    tag varchar(255),
+    archived boolean,
+    stage varchar(255),
+    status varchar(255),
+    error_message jsonb,
+    version varchar(255),
+    inserted_at timestamp
+    with
+        time zone DEFAULT CURRENT_TIMESTAMP,
+        updated_at timestamp
+    with
+        time zone DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id)
+);
+
