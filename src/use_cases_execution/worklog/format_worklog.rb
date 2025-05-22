c4be95733b7bb878f1e5183093bf6ccd4b589ff0
Juan Camilo Muñoz Valencia <juanc.munozv021@gmail.com>
2025-05-22 11:19:29 -0500

    feat(worklog): implement daily worklog notifications

---
+++ src/use_cases_execution/worklog/format_worklog.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'logger'
+require 'bas/shared_storage/postgres'
+
+require_relative '../../implementations/format_worklog'
+require_relative 'config'
+
+# Configuration
+read_options = {
+  connection: Config::CONNECTION,
+  db_table: 'worklog',
+  tag: 'FetchWorklogsFromNotion'
+}
+
+write_options = {
+  connection: Config::CONNECTION,
+  db_table: 'worklog',
+  tag: 'FormatWorklogs'
+}
+
+options = {
+  person_section_template: '**<person_name>**',
+  worklog_item_template: '- <hours>h: <activity>',
+  no_activity_message: 'Sin actividad especificada'
+}
+
+# Process bot
+begin
+  shared_storage = Bas::SharedStorage::Postgres.new({ read_options:, write_options: })
+
+  Implementation::FormatWorklogs.new(options, shared_storage).execute
+rescue StandardError => e
+  Logger.new($stdout).info(e.message)
+end
