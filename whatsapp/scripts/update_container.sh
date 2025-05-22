c1d6c52cc66d12b60caafc2fcc84941336a62b1d
juanhiginio <juanhiginio06@gmail.com>
2025-05-22 17:17:46 -0500

    chore: Delete WhatsApp folder

--- whatsapp/scripts/update_container.sh
+++
@@ -1,9 +0,0 @@
-echo "Starting update container script"
-
-# ACTIVATE BOT
-echo "Starting whatsapp webhook"
-ruby /app/bin/webhook.rb &
-
-# UPDATE CRONJOBS
-echo "Starting cronjobs"
-ruby /app/bin/execute_orchestrator.rb
