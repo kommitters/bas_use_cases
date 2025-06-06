948086f0a081d1675260a4399e287e1990eb03e7
Juan Camilo Muñoz Valencia <juanc.munozv021@gmail.com>
2025-06-06 10:35:05 -0500

    feat: implement uuid for the migration tables, and change the singular table name

--- db/warehouse_migrations/20250604174518_create_project_table.rb
+++ db/warehouse_migrations/20250604174518_create_project_table.rb
@@ -2,19 +2,19 @@
 
 Sequel.migration do
   up do
-    create_table(:project) do
-      primary_key :id, size: 255
+    create_table(:projects) do
+      uuid :id, primary_key: true, default: Sequel.lit('gen_random_uuid()')
       String :external_project_id, size: 255, null: false
-      String :name, null: false
+      String :name, size: 255, null: false
       String :type, size: 100, null: false
-      String :weekly_scope_id, size: 255, null: true
-      String :domain_id, size: 255, null: true
+      String :external_weekly_scope_id, size: 255, null: true
+      String :external_domain_id, size: 255, null: true
       DateTime :created_at, default: Sequel.lit('CURRENT_TIMESTAMP')
       DateTime :updated_at, default: Sequel.lit('CURRENT_TIMESTAMP')
     end
   end
 
   down do
-    drop_table(:project)
+    drop_table(:projects)
   end
 end
--- db/warehouse_migrations/20250604181311_create_activity_table.rb
+++ db/warehouse_migrations/20250604181311_create_activity_table.rb
@@ -2,17 +2,17 @@
 
 Sequel.migration do
   up do
-    create_table(:activity) do
-      primary_key :id, size: 255
+    create_table(:activities) do
+      uuid :id, primary_key: true, default: Sequel.lit('gen_random_uuid()')
       String :external_activity_id, size: 255, null: false
       String :name, null: false
-      String :domain_id, size: 255, null: true
+      String :external_domain_id, size: 255, null: true
       DateTime :created_at, default: Sequel.lit('CURRENT_TIMESTAMP')
       DateTime :updated_at, default: Sequel.lit('CURRENT_TIMESTAMP')
     end
   end
 
   down do
-    drop_table(:activity)
+    drop_table(:activities)
   end
 end
