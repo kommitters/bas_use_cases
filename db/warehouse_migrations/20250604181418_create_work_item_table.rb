f116d56065624a3cf9e997aa02d301f0b2fc7252
Juan Camilo Muñoz Valencia <juanc.munozv021@gmail.com>
2025-06-06 10:35:05 -0500

    feat: implement uuid for the migration tables, and change the singular table name

--- db/warehouse_migrations/20250604181418_create_work_item_table.rb
+++ db/warehouse_migrations/20250604181418_create_work_item_table.rb
@@ -2,22 +2,21 @@
 
 Sequel.migration do
   up do
-    create_table(:work_item) do
-      primary_key :id, size: 255
+    create_table(:work_items) do
+      uuid :id, primary_key: true, default: Sequel.lit('gen_random_uuid()')
       String :external_work_item_id, size: 255, null: false
-      foreign_key :project_id, :project, size: 255, null: true
-      foreign_key :activity_id, :activity, size: 255, null: true
-      String :assignee_person_id, size: 255, null: true
+      foreign_key :project_id, :projects, type: :uuid, null: true
+      foreign_key :activity_id, :activities, type: :uuid, null: true
       String :external_domain_id, size: 255, null: true
       String :external_weekly_scope_id, size: 255, null: true
       String :work_item_status, size: 50, null: false
-      DateTime :work_item_completetion_date, null: true
+      DateTime :work_item_completion_date, null: true
       DateTime :created_at, default: Sequel.lit('CURRENT_TIMESTAMP')
       DateTime :updated_at, default: Sequel.lit('CURRENT_TIMESTAMP')
     end
   end
 
   down do
-    drop_table(:work_item)
+    drop_table(:work_items)
   end
 end
