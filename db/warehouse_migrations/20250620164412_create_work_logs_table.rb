30fc592f411f8f68aea3a3e3c2b8c9d53228b7ed
cammv21 <juanc.munozv021@gmail.com>
2025-06-20 18:04:42 -0500

    feat: add migration to create work logs table

---
+++ db/warehouse_migrations/20250620164412_create_work_logs_table.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  up do
+    create_table(:work_logs) do
+      uuid :id, primary_key: true, default: Sequel.lit('gen_random_uuid()')
+      String :external_work_log_id, size: 255, null: false
+      Integer :duration_minutes, null: false
+      column :tags, 'text[]', null: true
+      foreign_key :person_id, :persons, type: :uuid, null: false, on_delete: :cascade
+      foreign_key :project_id, :projects, type: :uuid, null: true, on_delete: :set_null
+      foreign_key :activity_id, :activities, type: :uuid, null: true, on_delete: :set_null
+      foreign_key :work_item_id, :work_items, type: :uuid, null: true, on_delete: :set_null
+      DateTime :creation_date, null: false
+      DateTime :modification_date, null: true
+      DateTime :created_at, default: Sequel.lit('CURRENT_TIMESTAMP')
+      DateTime :updated_at, default: Sequel.lit('CURRENT_TIMESTAMP')
+    end
+  end
+
+  down do
+    drop_table(:work_logs)
+  end
+end
