05c86646872aa8f2be2c0228841e45eca9c8807c
cammv21 <juanc.munozv021@gmail.com>
2025-07-03 15:13:49 -0500

    feat: implement formatter for hired person data

---
+++ src/utils/warehouse/notion/hired_person_formatter.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative './base'
+
+module Utils
+  module Warehouse
+    module Notion
+      module Formatter
+        #
+        # Formats records from the "Hired" database using pre-fetched warehouse data.
+        class HiredPersonFormatter < Base
+          # Formats a list of Notion records.
+          def format
+            {
+              full_name: extract_title('Name'),
+              email_address: extract_email('Email'),
+              hired_date: extract_date('Beginning Date'),
+              exit_date: extract_date('End Date'),
+              role: extract_select('Role'),
+              external_domain_id: extract_relation('Domains').first
+            }
+          end
+        end
+      end
+    end
+  end
+end
