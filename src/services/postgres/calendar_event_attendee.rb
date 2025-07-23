9f71022bb9cec38eedba4091d7f7d2e55bc1e728
cammv21 <juanc.munozv021@gmail.com>
2025-07-23 11:17:00 -0500

    refactor: update the calendar event attendee service to manage the relation with persons

--- src/services/postgres/calendar_event_attendee.rb
+++ src/services/postgres/calendar_event_attendee.rb
@@ -2,6 +2,7 @@
 
 require_relative 'base'
 require_relative 'calendar_event'
+require_relative 'person'
 
 module Services
   module Postgres
@@ -10,7 +11,7 @@ module Services
     #
     # Provides CRUD operations for the 'calendar_event_attendees' table.
     class CalendarEventAttendee < Services::Postgres::Base
-      ATTRIBUTES = %i[calendar_event_id email response_status].freeze
+      ATTRIBUTES = %i[calendar_event_id person_id email response_status].freeze
 
       TABLE = :calendar_event_attendees
 
@@ -20,6 +21,8 @@ module Services
 
       def insert(params)
         assign_relations(params)
+        assign_person_from_email(params)
+
         transaction { insert_item(TABLE, params) }
       rescue StandardError => e
         handle_error(e)
@@ -29,6 +32,8 @@ module Services
         raise ArgumentError, 'Calendar event attendee id is required to update' unless id
 
         assign_relations(params)
+        assign_person_from_email(params)
+
         transaction { update_item(TABLE, id, params) }
       rescue StandardError => e
         handle_error(e)
@@ -53,6 +58,15 @@ module Services
       end
 
       private
+
+      def assign_person_from_email(params)
+        return unless params[:email]
+
+        person_service = Services::Postgres::Person.new(db)
+        person = person_service.query(email_address: params[:email]).first
+
+        params[:person_id] = person[:id] if person
+      end
 
       def handle_error(error)
         puts "[CalendarEventAttendee Service ERROR] #{error.class}: #{error.message}"
