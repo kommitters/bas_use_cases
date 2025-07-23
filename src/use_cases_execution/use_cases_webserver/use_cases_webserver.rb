6d7e078dd53322bc44bac8aba55c3fe5cf41a3f9
David L <cadalagu@hotmail.com>
2025-07-23 16:50:38 -0500

    Fix: Remove unnecessary lines from use_cases_webserver.

--- src/use_cases_execution/use_cases_webserver/use_cases_webserver.rb
+++ src/use_cases_execution/use_cases_webserver/use_cases_webserver.rb
@@ -3,12 +3,12 @@
 require 'sinatra/base'
 require_relative '../pto/fetch_pto_from_google_for_workspace'
 
-# The WebServer class defines the main Sinatra application responsible for                                       │
+# The WebServer class defines the main Sinatra application responsible for
 # handling incoming webhooks from Google services.
-#                                                                                                               │
-# This server acts as a central entry point and delegates the handling of                                        │
-# specific routes to the `Routes` class, which contains the definitions                                          │
-# for all available endpoints.                                                                                   │
+
+# This server acts as a central entry point and delegates the handling of
+# specific routes to the `Routes` class, which contains the definitions
+# for all available endpoints.
 # WebServer is the main Sinatra application class.
 class WebServer < Sinatra::Base
   use Routes::Pto
