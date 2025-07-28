3842ac2bb1f609ddeb33d2a04e9ab8b27dcce59d
Juan Pablo BC <xleed@protonmail.ch>
2025-07-28 14:39:25 -0500

    Using to_json instead of sinatra json helper

--- src/use_cases_execution/google_documents/listen_to_google_docs_updates.rb
+++ src/use_cases_execution/google_documents/listen_to_google_docs_updates.rb
@@ -11,43 +11,35 @@ module Routes
   # Routes::GoogleDocuments defines the /google_documents endpoint that receives Google Documents data
   class GoogleDocuments < Sinatra::Base
     write_options = {
-      connection: Config::CONNECTION, db_table: 'google_documents', tag: 'FetchGoogleDocumentsFromGoogleWorkspace'
+      connection: Config::CONNECTION, db_table: 'warehouse_sync', tag: 'FetchGoogleDocumentsFromWorkspace'
     }
 
     shared_storage_writer = Bas::SharedStorage::Postgres.new(write_options: write_options)
 
     post '/google_docs' do
-      puts "\n\nrequest: #{request}"
       body = request.body.read.to_s
-      halt 400, json(error: 'Empty request body') if body.empty?
-
-      puts "body: #{body}"
-
-      if body.strip.empty?
-        status 400
-        return { error: 'Empty request body' }.to_json
-      end
+      halt 400, { error: 'Empty request body' }.to_json if body.strip.empty?
 
       data = begin
         JSON.parse(body)
       rescue StandardError
-        halt(400, json(error: 'Invalid JSON format'))
+        halt(400, { error: 'Invalid JSON format' }.to_json)
       end
 
       unless data.is_a?(Hash) && data['google_documents'].is_a?(Array)
-        halt 400, json(error: 'Missing or invalid "google_documents" array')
+        halt 400, { error: 'Missing or invalid "google_documents" array' }.to_json
       end
 
-      shared_storage_writer.write(success: { google_documents: data['google_documents'] })
+      shared_storage_writer.write(success: { type: 'document', content: data['google_documents'] })
       status 200
-      json(message: 'Google documents stored successfully')
+      { message: 'Google documents stored successfully' }.to_json
     rescue JSON::ParserError => e
       logger.error "Invalid JSON format: #{e.message}"
       status 400
       { error: 'Invalid JSON format' }.to_json
     rescue StandardError => e
       logger.error "Failed to process Google documents data: #{e.message}\n#{e.backtrace.join("\n")}"
-      halt(500, json(error: 'Internal Server Error'))
+      halt(500, { error: 'Internal Server Error' }.to_json)
     end
   end
 end
