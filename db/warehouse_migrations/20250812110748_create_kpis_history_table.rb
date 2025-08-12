e7bba2fc1e1dd19405dc87f0a601a2cc296dcb9b
cammv21 <juanc.munozv021@gmail.com>
2025-08-12 14:09:23 -0500

    feat: add migration file for kpis history table

---
+++ db/warehouse_migrations/20250812110748_create_kpis_history_table.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  up do
+    create_table(:kpis_history) do
+      uuid :id, primary_key: true, default: Sequel.lit('gen_random_uuid()')
+      foreign_key :kpi_id, :kpis, null: false, on_delete: :cascade, type: :uuid
+      String :external_kpi_id, size: 255, null: true
+      foreign_key :domain_id, :domains, null: false, on_delete: :cascade, type: :uuid
+      String :description, size: 255, null: true
+      String :status, size: 255, null: true
+      Float :current_value, null: true
+      Float :percentage, null: true
+      Float :target_value, null: true
+      jsonb :stats, null: true
+      DateTime :created_at, default: Sequel.lit('CURRENT_TIMESTAMP')
+      DateTime :updated_at, default: Sequel.lit('CURRENT_TIMESTAMP')
+    end
+  end
+
+  down do
+    drop_table(:kpis_history)
+  end
+end
