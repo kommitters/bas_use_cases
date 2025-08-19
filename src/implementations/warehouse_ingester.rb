7970fe9f149e176e9eab8cf9fb1b1f4c09a2f449
cammv21 <juanc.munozv021@gmail.com>
2025-08-19 12:33:50 -0500

    chore: fix warehouse ingester issue

--- src/implementations/warehouse_ingester.rb
+++ src/implementations/warehouse_ingester.rb
@@ -16,6 +16,7 @@ require_relative '../services/postgres/github_release'
 require_relative '../services/postgres/github_issue'
 require_relative '../services/postgres/github_pull_request'
 require_relative '../services/postgres/kpi'
+require_relative '../services/postgres/calendar_event'
 
 module Implementation
   ##
@@ -74,7 +75,8 @@ module Implementation
       'github_pull_request' => {
         service: Services::Postgres::GithubPullRequest, external_key: 'external_github_pull_request_id'
       },
-      'kpi' => { service: Services::Postgres::Kpi, external_key: 'external_kpi_id' }
+      'kpi' => { service: Services::Postgres::Kpi, external_key: 'external_kpi_id' },
+      'calendar_event' => { service: Services::Postgres::CalendarEvent, external_key: 'external_calendar_event_id' }
 
     }.freeze
 
