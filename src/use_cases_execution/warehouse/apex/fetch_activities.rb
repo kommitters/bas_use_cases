4833fbe2ea6e8ec652d356bcd8e254824e9c1d8d
cammv21 <juanc.munozv021@gmail.com>
2025-09-12 17:13:07 -0500

    feat: add test files and execute files

---
+++ src/use_cases_execution/warehouse/apex/fetch_activities.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'logger'
+require 'bas/shared_storage/postgres'
+
+require_relative '../../../implementations/fetch_records_from_apex_database'
+require_relative '../config'
+
+read_options = {
+  connection: Config::Database::CONNECTION,
+  db_table: 'warehouse_sync',
+  avoid_process: true,
+  where: 'archived=$1 AND tag=$2 ORDER BY inserted_at DESC',
+  params: [false, 'FetchActivitiesFromApex']
+}
+
+write_options = {
+  connection: Config::Database::CONNECTION,
+  db_table: 'warehouse_sync',
+  tag: 'FetchActivitiesFromApex'
+}
+
+process_options = {
+  entity: 'activity',
+  endpoint: 'activities'
+}
+
+begin
+  shared_storage = Bas::SharedStorage::Postgres.new({ read_options:, write_options: })
+
+  Implementation::FetchRecordsFromApexDatabase.new(process_options, shared_storage).execute
+
+  Logger.new($stdout).info('Successfully fetched activities from APEX.')
+rescue StandardError => e
+  Logger.new($stdout).error("Failed to fetch activities from APEX: #{e.message}")
+end
