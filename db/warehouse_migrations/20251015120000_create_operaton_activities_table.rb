6720a8ded85fe47186b0b0a21fd5139324112406
David Roldán <deroldan98@gmail.com>
2025-10-15 10:48:31 -0500

    update: operaton tables migrations

--- db/warehouse_migrations/20251015120000_create_operaton_activities_table.rb
+++ db/warehouse_migrations/20251015120000_create_operaton_activities_table.rb
@@ -2,23 +2,25 @@
 
 Sequel.migration do
   up do
-    create_table(:operaton_processes) do
+    create_table(:operaton_activities) do
       uuid :id, primary_key: true, default: Sequel.lit('gen_random_uuid()')
-      String :external_process_id, size: 255, null: false, unique: true
-      String :business_key, size: 255
+      String :external_activity_id, size: 255, null: false, unique: true
+      foreign_key :external_process_id, :operaton_processes, key: :external_process_id, type: 'varchar(255)'
       String :process_definition_key, size: 255
-      String :process_definition_name, size: 255
+      String :activity_id, size: 255
+      String :activity_name
+      String :activity_type
+      String :task_id, size: 255
+      String :assignee
       DateTime :start_time
       DateTime :end_time
       Bignum :duration_in_millis
-      Integer :process_definition_version
-      String :state, size: 100
       DateTime :created_at, default: Sequel.lit('CURRENT_TIMESTAMP')
       DateTime :updated_at, default: Sequel.lit('CURRENT_TIMESTAMP')
     end
   end
 
   down do
-    drop_table(:operaton_processes)
+    drop_table(:operaton_activities)
   end
 end
