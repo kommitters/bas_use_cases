bb167b6f0d978875b47e46d7bd3fb8d283ade4ad
David Roldán <deroldan98@gmail.com>
2025-10-15 16:25:48 -0500

    fix: remove foreign key statements form migrations - since it could retrieve an error if the incident or activity if from an ongoing process

--- db/warehouse_migrations/20251015120000_create_operaton_activities_table.rb
+++ db/warehouse_migrations/20251015120000_create_operaton_activities_table.rb
@@ -5,7 +5,7 @@ Sequel.migration do
     create_table(:operaton_activities) do
       uuid :id, primary_key: true, default: Sequel.lit('gen_random_uuid()')
       String :external_activity_id, size: 255, null: false, unique: true
-      foreign_key :external_process_id, :operaton_processes, key: :external_process_id, type: 'varchar(255)'
+      String :external_process_id, size: 255, null: false
       String :process_definition_key, size: 255
       String :activity_id, size: 255
       String :activity_name
--- db/warehouse_migrations/20251015130000_create_operaton_incidents_table.rb
+++ db/warehouse_migrations/20251015130000_create_operaton_incidents_table.rb
@@ -5,7 +5,7 @@ Sequel.migration do
     create_table(:operaton_incidents) do
       uuid :id, primary_key: true, default: Sequel.lit('gen_random_uuid()')
       String :external_incident_id, size: 255, null: false, unique: true
-      foreign_key :external_process_id, :operaton_processes, key: :external_process_id, type: 'varchar(255)'
+      String :external_process_id, size: 255, null: false
       String :process_definition_key, size: 255
       String :activity_id, size: 255
       String :incident_type, size: 255
