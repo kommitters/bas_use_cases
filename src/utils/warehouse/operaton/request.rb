c9477247df33dd1c0d1bc8fbb6fa98a890aac631
David Roldán <deroldan98@gmail.com>
2025-10-24 17:49:11 -0500

    refactor: set `base_url` and `basic_auth` to lazy initialization and validation

--- src/utils/warehouse/operaton/request.rb
+++ src/utils/warehouse/operaton/request.rb
@@ -10,26 +10,39 @@ module Utils
     # Encapsulates API calls to the Operaton service.
     class Request
       include HTTParty
-      @base_url = "#{Config::Operaton::BASE_URI}/engine-rest"
-      @basic_auth = {
-        username: Config::Operaton::USER_ID,
-        password: Config::Operaton::PASSWORD_SECRET
-      }
-
-      def self.execute(endpoint:, method: :get, query_params: {}, body: {})
-        url = "#{@base_url}/#{endpoint}"
-        options = build_request_options(method: method, query: query_params, body: body)
-        HTTParty.public_send(method, url, options)
-      end
-
       class << self
+        def execute(endpoint:, method: :get, query_params: {}, body: {})
+          url = "#{base_url}/#{endpoint}"
+          options = build_request_options(method: method, query: query_params, body: body)
+          HTTParty.public_send(method, url, options)
+        end
+
+        def base_url
+          @base_url ||= begin
+            base_uri = Config::Operaton::BASE_URI
+            raise 'Operaton BASE_URI not configured' if base_uri.nil? || base_uri.empty?
+
+            "#{base_uri}/engine-rest"
+          end
+        end
+
+        def basic_auth
+          @basic_auth ||= begin
+            user_id = Config::Operaton::USER_ID
+            password = Config::Operaton::PASSWORD_SECRET
+            raise 'Operaton credentials not configured' if user_id.nil? || password.nil?
+
+            { username: user_id, password: password }
+          end
+        end
+
         private
 
         def build_request_options(method:, query:, body:)
           options = {
             query: query,
             timeout: 20,
-            basic_auth: @basic_auth
+            basic_auth: basic_auth
           }
 
           if %i[post put patch].include?(method)
